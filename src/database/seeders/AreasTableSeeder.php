992e339a649eb1f0af342a1c8cddb33c0ea629fa
eto0831 <eto0831@gmail.com>
2024-11-02 17:57:14 +0900

    Shops Genres Areas TableSeeder added

---
+++ src/database/seeders/AreasTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AreasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'area_name' => '東京',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'area_name' => '大阪',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'area_name' => '福岡',
+        ];
+        DB::table('areas')->insert($param);
+
+    }
+}
---
+++ src/database/seeders/GenresTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class GenresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'genre_name' => '寿司',
+        ];
+        DB::table('genres')->insert($param);
+
+        $param = [
+            'genre_name' => '焼肉',
+        ];
+        DB::table('genres')->insert($param);
+
+        $param = [
+            'genre_name' => '居酒屋',
+        ];
+        DB::table('genres')->insert($param);
+
+        $param = [
+            'genre_name' => 'イタリアン',
+        ];
+        DB::table('genres')->insert($param);
+
+        $param = [
+            'genre_name' => 'ラーメン',
+        ];
+        DB::table('genres')->insert($param);
+    }
+}
