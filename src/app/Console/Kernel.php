46298dcfddfaaf492b938c141495cb60e69caf55
eto0831 <eto0831@gmail.com>
2024-11-25 16:22:04 +0900

    deleted unneeded files related to sheduler and Supervisor

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('emails:send-reminders')->dailyAt('15:56');
+        $schedule->command('emails:send-reminders')->dailyAt('16:11');
     }
 
     /**
