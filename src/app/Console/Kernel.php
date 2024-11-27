9dc0708156567ba4408c475bb968d21d8966e4bf
eto0831 <eto0831@gmail.com>
2024-11-27 16:50:30 +0900

    command of queue removed from .yml and leaved supervisor.conf only

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('emails:send-reminders')->dailyAt('16:03');
+        $schedule->command('emails:send-reminders')->dailyAt('16:47');
     }
 
     /**
