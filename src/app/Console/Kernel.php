d67d7fae030dfbacbd1247f3f64173309f19e0ea
eto0831 <eto0831@gmail.com>
2024-11-27 17:08:42 +0900

    time fixed of supervisor and scheduler

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('emails:send-reminders')->dailyAt('16:47');
+        $schedule->command('emails:send-reminders')->dailyAt('17:06');
     }
 
     /**
